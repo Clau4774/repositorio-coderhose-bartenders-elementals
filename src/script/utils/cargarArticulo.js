a99f7400aaf98a9a132d876ad14ef2c455bc1bea
Claudio <cladiodangiolo@gmail.com>
2025-06-22 23:23:20 -0300

    Trabajando en función cargarArticulo.js

---
+++ src/script/utils/cargarArticulo.js
@@ -0,0 +1,10 @@
+const cargarArticulo = (articulos, articulo) => {
+    const listaDeArticulos = document.querySelector('#article-title');
+    const articleImage = document.querySelector('#article-image');
+    
+
+    const articuloBuscado = articulos.find(elem => elem.title === articulo.title);
+
+
+
+}
