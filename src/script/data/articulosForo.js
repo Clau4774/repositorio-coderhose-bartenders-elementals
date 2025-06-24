5d4b7c99e2e5ffe78b0c2a5460b32feb76882e67
Claudio <cladiodangiolo@gmail.com>
2025-06-24 14:28:31 -0300

    Se agregó categorias a los tragos

--- src/script/data/articulosForo.js
+++ src/script/data/articulosForo.js
@@ -1,6 +1,7 @@
 export default [
   {
     title: "El negroni",
+    category: ['clásico', 'clasico', 'fuerte', 'gin', 'vermouth', 'vermouth rosso', 'campari', 'amargo'],
     imageInfo: {
       imagePath: "https://assets.tmecosys.com/image/upload/t_web_rdp_recipe_584x480_1_5x/img/recipe/ras/Assets/1D4CCB7D-D830-4ED3-9535-875D10CFC801/Derivates/DAD3AE52-E326-4309-90BA-10F6BEEB1EC7.jpg",
       imageAltText: "foto de un negroni"
@@ -50,6 +51,7 @@ export default [
   },
   {
     title: "Como hacer un buen Mojito",
+    category: ['clásico', 'clasico', 'suave', 'refrescante', 'gasificado', 'ron', 'ron blanco'],
     imageInfo: {
       imagePath: "https://www.saveur.com/uploads/2007/02/SAVEUR_Mojito_1149-Edit-scaled.jpg?auto=webp",
       imageAltText: "Foto de un Mojito"
@@ -106,6 +108,7 @@ export default [
   },
     {
     title: "El penicillin: Un clásico moderno.",
+    category: ['moderno', 'clasico', 'clásico', 'fuerte', 'sour', 'whisky', 'whisky escoces', 'ahumado'],
     imageInfo: {
       imagePath: "https://www.liquor.com/thmb/FsPcbBZ5fnQy3mWgZ-xDPqxeS0Y=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/penicillin-720x720-primary-8157bd54c63948c4b5a3ad5b3986965a.jpg",
       imageAltText: "Foto de un Penicillin"
@@ -160,6 +163,7 @@ export default [
   },
   {
     title: "Un clásico, de clásicos. El Manhattan",
+    category: ['clasico', 'clásico', 'fuerte', 'whisky escoces', 'whisky', 'vermouth', 'vermouth rosso', 'amargo', 'amargo de angostura'],
     imageInfo: {
       imagePath: "https://tragos.washkuapp.com/cdn-cgi/imagedelivery/APNgvbEQnqs0QABTHivMOg/e3b3389c-0bdf-483d-8460-4c697098a400/w=240",
       imageAltText: "Foto de un Manhattan"
