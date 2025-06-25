49a412e9ef0c3b299ca9adbf5fdc7a5befa3bd36
claudioDang47 <claudiodangiolo@gmail.com>
2025-06-25 15:15:04 -0300

    Creado cargarImagenGaleria.js. Falta realizar funcionalidades

--- src/script/pages/galeria.js
+++ src/script/pages/galeria.js
@@ -1,6 +1,9 @@
 import { buscador } from '../utils/buscador.js';
+import { cargarImagenGaleria } from '../utils/cargarImagenGaleria.js';
 
 const inputBusqueda = document.querySelector('#input-busqueda');
 
 inputBusqueda.addEventListener('input', buscador);
+
+
 
---
+++ src/script/utils/cargarImagenGaleria.js
@@ -0,0 +1,5 @@
+import { encontrarArticulo } from './encontrarArticulo.js'
+
+export const cargarImagenGaleria = () => {
+    console.log('está')
+}
