b41a5d1f35fdebb2e9fa0053f137e3fe3b9b4e8f
claudioDang47 <claudiodangiolo@gmail.com>
2025-06-25 12:28:12 -0300

    Se modificó el criterio de búsqueda, para que encuentre haciendo la comparación con el title del elemento

--- src/script/utils/encontrarArticulo.js
+++ src/script/utils/encontrarArticulo.js
@@ -2,7 +2,7 @@ import articulosForo from "../data/articulosForo.js";
 
 export const encontrarArticulo = (option = undefined) => {    
     if(option !== undefined && option.trim() !== '') {
-        const filteredArticles = articulosForo.filter(elem => elem.name.includes(option.toLowerCase()));
+        const filteredArticles = articulosForo.filter(elem => elem.title.toLocaleLowerCase().includes(option.toLowerCase()));
         console.log(filteredArticles, "filteredArticles");
         console.log(option, 'option')
 
