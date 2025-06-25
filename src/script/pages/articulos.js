e16186081f372fe05872e30b26f1ccc279d31939
claudioDang47 <claudiodangiolo@gmail.com>
2025-06-25 11:52:03 -0300

    Se agrego la funcionalidad de búsqueda a articulos.js ya que no se había sumado funcionalidad de búsqueda

--- src/script/pages/articulos.js
+++ src/script/pages/articulos.js
@@ -1,4 +1,9 @@
 import { cargarArticulos } from "../utils/cargarArticulos.js";
+import { buscador } from '../utils/buscador.js';
+
+const inputBusqueda = document.querySelector('#input-busqueda');
+
+inputBusqueda.addEventListener('input', buscador);
 
 const lastArticlesContainer = document.querySelector("#last-articles-container");
 
